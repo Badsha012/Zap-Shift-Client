2ab3baa4d6700442df6461552fb2ef88daf2586e
Badsha012 <badshasheikh361@gmail.com>
2025-11-17 09:24:25 +0600

    adderd a navbar or footer

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,8 +1,17 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home/Home/Home";
 
  export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>
+    element:<Root></Root>,
+    children:[
+        {
+            index:true,
+            element:<Home></Home>
+        }
+
+    ]
   },
 ]);
