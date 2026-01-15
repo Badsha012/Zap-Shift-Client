00a718e8c30aea62c659a1760db2c5265c984099
Badsha012 <badshasheikh361@gmail.com>
2026-01-15 10:06:01 +0600

    added a auth provider

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -1,33 +1,70 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../../hooks/useAuth";
+import { Link } from "react-router";
 
 const Login = () => {
-  const {register,handleSubmit}=useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleLogin=(data) =>{
-    console.log("from data",data);
+  const { signInUser } = useAuth();
 
-  }
+  const handleLogin = (data) => {
+    console.log("from data", data);
+    signInUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        
-             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+  return (
+    <div className="card bg-base-100 w-full max-w-sm mx-auto shrink-0 shadow-2xl">
+        <h3 className="text-3xl text-center">Wlecome back</h3>
+        <p className="text-center">Please Login</p>
       <form className="card-body" onSubmit={handleSubmit(handleLogin)}>
         <fieldset className="fieldset">
-            {/* email field */}
+          {/* email field */}
           <label className="label">Email</label>
-          <input type="email" {...register('email')} className="input" placeholder="Email" />
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is Required</p>
+          )}
+
           {/* password field */}
           <label className="label">Password</label>
-          <input type="password"{...register('password')} className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+
+          {errors.password?.type === "minlength" && (
+            <p className="text-red-500">
+              Password must be 6 character or longer
+            </p>
+          )}
+
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
           <button className="btn btn-neutral mt-4">Login</button>
         </fieldset>
+        <p>New to Zap Shift <Link className="text-blue-400 underline" to="/register">Register</Link></p>
       </form>
     </div>
-            
-       
-    );
+  );
 };
 
 export default Login;
--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,54 +1,81 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import useAuth from '../../../hooks/useAuth';
+import React from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../../hooks/useAuth";
+import { Link } from "react-router";
 
 const Register = () => {
-    const { register,handleSubmit,formState:{errors}}=useForm();
-     const { registerUser}=useAuth()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { registerUser } = useAuth();
 
-        const handleRegistration=(data)=>{
-            console.log('after register',data);
-            registerUser(data.email,data.password)
-            .then(result=>{
-              console.log(result.user);
-            })
-            .catch(error=>{
-              console.log(error);
-              
-            })
+  const handleRegistration = (data) => {
+    console.log("after register", data);
+    registerUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        }
-
-    return (
-        <div>
-            <form  onSubmit={handleSubmit(handleRegistration)}>
-                
+  return (
+    <div className="card bg-base-100 w-full max-w-sm mx-auto shrink-0 shadow-2xl">
+      <h3 className="text-3xl text-center">Wlecome back</h3>
+      <p className="text-center">Please Login</p>
+      <form className="card-body" onSubmit={handleSubmit(handleRegistration)}>
         <fieldset className="fieldset">
           <label className="label">Email</label>
-          <input type="email" {...register('email',{required:true})} className="input" placeholder="Email" />
-             {
-                errors.email?.type==='required' && <p className='text-red-500'>Email is Required</p>
-             }
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is Required</p>
+          )}
           {/* password */}
           <label className="label">Password</label>
-          <input type="password" {...register('password',{required:true,minLength:6,pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#]).+$/})} className="input" placeholder="Password" />
-          {
-            errors.password?.type==='required' && <p className='text-red-700'>Password is Required</p>
-
-          }
-          {
-            errors.password?.type=== 'minLength'&& <p className='text-red-600'>Password is  characters o longer</p>
-          }
-          {
-            errors.password?.type ==='pattern' && <p className='text-red-500'>Password must hava at least one Upper case,Atleast one lower case,at least one spacial character,at least one number </p>
-          }
-          <div><a className="link link-hover">Forgot password?</a></div>
+          <input
+            type="password"
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#]).+$/,
+            })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-700">Password is Required</p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-600">Password is characters o longer</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500">
+              Password must hava at least one Upper case,Atleast one lower
+              case,at least one spacial character,at least one number{" "}
+            </p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
           <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
-            </form>
-            
-        </div>
-    );
+        <p>
+          Already Have an Account{" "}
+          <Link className="text-blue-400 underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
