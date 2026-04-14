eea563dff52691a5a9729140b0dfbd1805403860
Badsha012 <badshasheikh361@gmail.com>
2026-04-14 11:25:51 +0600

    added a about section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,14 +2,49 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="py-10">
+    <div className="bg-white px-10 py-10 rounded-xl">
       <h1 className="text-5xl font-bold">About Us</h1>
-      <p>
+      <p className="text-gray-500">
         Enjoy fast, reliable parcel delivery with real-time tracking and zero
-        hassle. From personal packages to business shipments — we deliver on
-        time, every time.
+        hassle.<br></br> From personal packages to business shipments — we
+        deliver on time, every time.
       </p>
-      
+      <div className="py-10 ">
+        <ul className="text-xl font-bold flex justify-between">
+          <li>Stroy</li>
+          <li>Mission</li>
+          <li>Suceess</li>
+          <li>Teams & Other </li>
+          <li></li>
+        </ul>
+      </div>
+      <p className="text-gray-400">
+        We started with a simple promise — to make parcel delivery fast,
+        reliable, and stress-free. Over the years, our commitment to real-time
+        tracking, efficient logistics, and customer-first service has made us a
+        trusted partner for thousands. Whether it's a personal gift or a
+        time-sensitive business delivery, we ensure it reaches its destination —
+        on time, every time.
+      </p>
+      <br />
+      <p className="text-gray-400">
+        We started with a simple promise — to make parcel delivery fast,
+        reliable, and stress-free. Over the years, our commitment to real-time
+        tracking, efficient logistics, and customer-first service has made us a
+        trusted partner for thousands. Whether it's a personal gift or a
+        time-sensitive business delivery, we ensure it reaches its destination —
+        on time, every time.
+      </p>
+      <br>
+      </br>
+      <p className="text-gray-400">
+        We started with a simple promise — to make parcel delivery fast,
+        reliable, and stress-free. Over the years, our commitment to real-time
+        tracking, efficient logistics, and customer-first service has made us a
+        trusted partner for thousands. Whether it's a personal gift or a
+        time-sensitive business delivery, we ensure it reaches its destination —
+        on time, every time.
+      </p>
     </div>
   );
 };
